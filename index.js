8b88727ae74a1c35752bb91d3b74fd7a2eb3648e
tlaze <tomlazore@gmail.com>
2021-10-13 18:31:58 -0400

    manager questions added to team array

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Intern = require('./lib/intern');
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+const fullTeam = [];
 
 const employeeQuestions = () =>{
     inquirer
@@ -38,15 +39,15 @@ const employeeQuestions = () =>{
             switch(response.employeePosition){
                 case 'Manager':
                     console.log(`${response.employeeName} is a Manager`);
-                    managerQuestions();
+                    managerQuestions(response);
                     break;
                 case 'Engineer':
                     console.log(`${response.employeeName} is an Engineer`);
-                    engineerQuestions();
+                    engineerQuestions(response);
                     break;
                 case 'Intern':
                     console.log(`${response.employeeName} is an Intern`);
-                    internQuestions();
+                    internQuestions(response);
                     break;
                 default:
                     console.log("Employee Not Found");
@@ -54,24 +55,54 @@ const employeeQuestions = () =>{
         });
 }
 
-const managerQuestions = () =>{
+const managerQuestions = (initialQuestions) =>{
     inquirer.prompt ([
         {
             type: 'input',
             message: `What is the Manager's Office Number?`,
             name: 'officeNumber',
+        },
+        {
+            type: 'confirm',
+            message: `Employee created! Would you like to add a new employee?`,
+            name: 'newEmployee',
         }
     ])
+    .then((response) => {
+        const newManager = new Manager(initialQuestions.employeeName, initialQuestions.employeeID, initialQuestions.employeeEmail, initialQuestions.employeePosition, response.officeNumber)
+        fullTeam.push(newManager);
+        if(response.newEmployee === true){
+            console.log("Create new Employee");
+        }
+        else{
+            console.log("No more new employees");
+        }
+        console.log(`Full Team: ${fullTeam}`);
+    })
 }
 
 const engineerQuestions = () =>{
     inquirer.prompt ([
         {
             type:'input',
-            message: 'Enter their GitHub link',
+            message: 'Enter their GitHub username',
             name: 'github',
+        },
+        {
+            type: 'confirm',
+            message: `Employee created! Would you like to add a new employee?`,
+            name: 'newEmployee',
         }
     ])
+    .then((response) => {
+        if(response.newEmployee === true){
+            console.log("Create new Employee");
+        }
+        else{
+            console.log("No more new employees");
+        }
+    })
+    
 }
 
 const internQuestions = () => {
@@ -80,8 +111,21 @@ const internQuestions = () => {
             type: 'input',
             message: 'What is the name of their school?',
             name: 'schoolName',
+        },
+        {
+            type: 'confirm',
+            message: `Employee created! Would you like to add a new employee?`,
+            name: 'newEmployee',
         }
     ])
+    .then((response) => {
+        if(response.newEmployee === true){
+            console.log("Create new Employee");
+        }
+        else{
+            console.log("No more new employees");
+        }
+    })
 }
 
 employeeQuestions();

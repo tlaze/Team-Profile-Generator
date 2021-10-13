ac173d4416ab15f043241c5963c6884f7a9d11b3
tlaze <tomlazore@gmail.com>
2021-10-13 18:02:24 -0400

    each position's specific questions are linked to the list choice

--- index.js
+++ index.js
@@ -30,12 +30,58 @@ const employeeQuestions = () =>{
                 type: 'list',
                 message: "What is this Employee's position for this project?",
                 name: 'employeePosition',
-                choices: ['Manager', 'Engineer', 'Intern']
+                choices: ['Manager', 'Engineer', 'Intern'],
             },
         ])
         .then((response) => {
             console.log(response);
+            switch(response.employeePosition){
+                case 'Manager':
+                    console.log(`${response.employeeName} is a Manager`);
+                    managerQuestions();
+                    break;
+                case 'Engineer':
+                    console.log(`${response.employeeName} is an Engineer`);
+                    engineerQuestions();
+                    break;
+                case 'Intern':
+                    console.log(`${response.employeeName} is an Intern`);
+                    internQuestions();
+                    break;
+                default:
+                    console.log("Employee Not Found");
+            }
         });
+}
+
+const managerQuestions = () =>{
+    inquirer.prompt ([
+        {
+            type: 'input',
+            message: `What is the Manager's Office Number?`,
+            name: 'officeNumber',
+        }
+    ])
+}
+
+const engineerQuestions = () =>{
+    inquirer.prompt ([
+        {
+            type:'input',
+            message: 'Enter their GitHub link',
+            name: 'github',
+        }
+    ])
+}
+
+const internQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of their school?',
+            name: 'schoolName',
+        }
+    ])
 }
 
 employeeQuestions();
